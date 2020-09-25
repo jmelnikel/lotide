eea0c3aa7cc457c304d2c26f554f44dea1673bca
Jason Melnikel <jmelnikel@gmail.com>
2020-09-25 13:19:13 -0400

    assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// This function is given two primitive datatype values as arguments. It will print to the console if the values given triple equal or not.
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} triple equals ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} does not equal ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 10);
+assertEqual(1, "1");
