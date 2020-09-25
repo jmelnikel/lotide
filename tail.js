d0b1a41d9caa5e9df37a7121066bfd532de9a2ff
Jason Melnikel <jmelnikel@gmail.com>
2020-09-25 14:10:19 -0400

    tail

---
+++ tail.js
@@ -0,0 +1,26 @@
+// Here temporarily
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} does not equal ${expected}`);
+  }
+};
+
+
+// This function will take the tail of an array (everything except the first element) and return a new array of those elements.
+
+const words =  ["Yo Yo", "Lighthouse", "Labs"];
+
+const tail = function(array) {
+  let newArray = [];
+  for (let i = 1; i < array.length; i++) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+};
+
+assertEqual(words.length, 3);
+assertEqual(tail(words).length, 2);
+assertEqual(tail(words)[0], "Lighthouse");
+assertEqual(tail(words)[1], "Labs");
