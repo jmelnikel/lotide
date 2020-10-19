e4b39f0df0fd7ab5f388a1c96639c50c1180c3dc
Jason Melnikel <jmelnikel@gmail.com>
2020-10-19 15:45:21 -0400

    refactor head.js

--- head.js
+++ head.js
@@ -1,23 +1,4 @@
-// Here temporarily
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} does not equal ${expected}`);
-  }
-};
-
 // This function will take the first item of an array and return the value (not in an array).
-const head = function(array) {
-  return array[0];
-};
+const head = array => array[0];
 
-
-// Testing
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "hello");
-assertEqual(head([5, 6, 7]), 6);
-assertEqual(head([5]), 5);
-assertEqual(head([]), undefined);
-
+module.exports = head;
