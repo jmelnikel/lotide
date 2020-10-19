a4440c51c4b695a29514b2fa707e203c6cbc6f79
Jason Melnikel <jmelnikel@gmail.com>
2020-10-19 16:03:57 -0400

    refactor assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,8 @@
-// Here Temporarily
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let index in array1) {
-    if (array1[index] !== array2[index]) {
-      return false;
-    }
-  }
-  return true;
-};
-
 // Do two arrays triple equal?
 // This function is given two arrays as arguments. It will print to the console if the arrarys given equal or not. The equality is not just in content, but order and index position of the elements.
-const assertArraysEqual = function(actual, expected) {
+const eqArrays = require("./eqArrays")
+
+const assertArraysEqual = (actual, expected) => {
   if (!eqArrays(actual, expected)) {
     return console.log(`Assertion Failed: ${actual} does not equal ${expected}`);
   } else {
@@ -21,7 +10,5 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+
+module.exports = assertArraysEqual;
